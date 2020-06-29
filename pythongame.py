443dc02fe5a61ce9a759ab97f5497d48b03d77e0
HanJunSeo <hjs123@jbnu.ac.kr>
2020-06-29 21:04:30 +0900

    draw_rect

--- pythongame.py
+++ pythongame.py
@@ -1,21 +1,28 @@
-"""justwindow.py"""
+"""draw_rect1"""
 import sys
 import pygame
-from pygame.locals import QUIT
+from pygame.locals import QUIT, Rect
 
 pygame.init()
 SURFACE = pygame.display.set_mode((400, 300))
-pygame.display.set_caption("Just Window")
+FPSCLOCK = pygame.time.Clock()
 
 def main():
     while True:
-        SURFACE.fill((255, 255, 255))
-    for event in pygame.event.get():
-        if event == QUIT:
-            pygame.quit()
-            sys.exit()
-            pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        SURFACE.fill((255,255,255))
+        pygame.draw.rect(SURFACE, (255, 0, 0), (10, 20, 100, 50))
+        pygame.draw.rect(SURFACE, (255, 0, 0), (150, 10, 100, 30), 3)
+        pygame.draw.rect(SURFACE, (0, 255, 0), ((100, 80), (80, 50)))
+        rect0 = Rect(200, 60, 140, 80)
+        pygame.draw.rect(SURFACE, (0, 0, 255), rect0)
+        rect1 = Rect((30, 160), (100, 50))
+        pygame.draw.rect(SURFACE, (255, 255, 0), rect1)
 
+        pygame.display.update()
+        FPSCLOCK.tick(3)
 if __name__=="__main__":
     main()
-
